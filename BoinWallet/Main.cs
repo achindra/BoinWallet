465de21606183174de0b0b977ee307a2a7d0eb56
Achindra Bhatnagar <achindra@github.com>
2019-05-05 10:22:34 +0530

    BoinWallet

---
+++ BoinWallet/Main.cs
@@ -0,0 +1,19 @@
+﻿using UIKit;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace BoinWallet
+{
+    public class Application
+    {
+
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://boinwallet.azurewebsites.net");
+
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
